c29ea5345ec2e35c79bbb9e117ae3d5edc2d9fc1
jiangyan <yan.jiang@kindroid.com>
2018-04-22 16:20:21 +0800

    调试多页面编译配置

---
+++ src/modules/b/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h2>{{message}}</h2>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      message: 'b模块的内容'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/modules/b/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2018/4/22.
+ */
+import Vue from 'vue'
+import App from './App'
+// import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  // router,
+  render: h => h(App)
+})
